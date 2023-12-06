4195f1950ec4b97d738510e5e4f4be01549fbffc
Antonio Gean <antgean@gmail.com>
2023-12-05 21:40:13 -0300

    add second migration

--- SallesWebMVC/Data/SallesWebMVCContext.cs
+++ SallesWebMVC/Data/SallesWebMVCContext.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SallesWebMVC.Models;
 
 namespace SallesWebMVC.Data
 {
     public class SallesWebMVCContext : DbContext
     {
-        public SallesWebMVCContext (DbContextOptions<SallesWebMVCContext> options)
+        public SallesWebMVCContext(DbContextOptions<SallesWebMVCContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SallesWebMVC.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; } = default!;
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
     }
 }
---
+++ SallesWebMVC/Models/Seller.cs
@@ -0,0 +1,37 @@
+﻿namespace SallesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set;}
+        public double BaseSalary { get;}
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord record)
+        {
+            SalesRecords.Add(record);
+        }
+
+        public double TotalSales(DateTime initialDate, DateTime finalDate) 
+        {
+            return SalesRecords.Where(records => records.Date >= initialDate && records.Date <= finalDate).Sum(records => records.Amount);
+        }
+    }
+}
