07fe8108e7beb900771a1d48b9b4df90779990ee
Antonio Gean <antgean@gmail.com>
2024-01-11 22:07:12 -0300

    adiciona validação

--- SallesWebMVC/Models/Seller.cs
+++ SallesWebMVC/Models/Seller.cs
@@ -7,20 +7,26 @@ namespace SallesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         public string Name { get; set; }
 
-
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display( Name = "Data de aniversário")]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set;}
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Salário base")]
         [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
